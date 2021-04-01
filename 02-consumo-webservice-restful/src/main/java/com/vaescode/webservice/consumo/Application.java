62c55acb468bc987926907e2ee5e6e0cd33401ff
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-04-01 12:36:39 -0600

    RestTemplate: ejemplo de consumo de un web service restful

---
+++ 02-consumo-webservice-restful/src/main/java/com/vaescode/webservice/consumo/Application.java
@@ -0,0 +1,35 @@
+package com.vaescode.webservice.consumo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import com.vaescode.webservice.consumo.entity.Cita;
+
+@SpringBootApplication
+public class Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Cita cita = restTemplate.getForObject("https://quoters.apps.pcfone.io/api/random", Cita.class);
+			log.info(cita.toString());
+		};
+	}
+}
