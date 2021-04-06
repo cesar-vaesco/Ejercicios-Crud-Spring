f774e323f89e6921539affd8a9cfbd3e24308417
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-04-06 12:28:18 -0500

    Inicializando proyecto y conectando el mismo a la base de datos

---
+++ Rest-Service/src/main/java/com/vaescode/backend/rest/RestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vaescode.backend.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+}
---
+++ Rest-Service/src/test/java/com/vaescode/backend/rest/RestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaescode.backend.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
