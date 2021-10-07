85389f2902c44f1b348317aa15da7b8a9e8f5f03
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-10-07 18:06:31 -0500

    DEMO: Editadno la vista de el archivo usuarios.html

--- demo/src/main/java/com/vaescode/demo/controllers/UsuarioController.java
+++ demo/src/main/java/com/vaescode/demo/controllers/UsuarioController.java
@@ -1,14 +1,70 @@
 package com.vaescode.demo.controllers;
 
+import com.vaescode.demo.models.Usuario;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "prueba")
-    public String pruebla (){
-        return "Prueba";
-    }
+	@RequestMapping(value = "usuario/{id}")
+	public Usuario getUsuario(@PathVariable Long id) {
+		
+		Usuario usuario = new Usuario();
+		usuario.setId(id);
+		usuario.setNombre("César");
+		usuario.setApellido("Vargas");
+		usuario.setEmail("cesar@correo.com");
+		usuario.setTelefono("5568000916");
+		usuario.setPassword("contrasenia");
+		return usuario;
+	}
+
+	
+	public Usuario listarUsuarios() {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("César");
+		usuario.setApellido("Vargas");
+		usuario.setEmail("cesar@correo.com");
+		usuario.setTelefono("5568000916");
+		usuario.setPassword("contrasenia");
+		return usuario;
+	}
+
+	
+	public Usuario crearUsuario() {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("César");
+		usuario.setApellido("Vargas");
+		usuario.setEmail("cesar@correo.com");
+		usuario.setTelefono("5568000916");
+		usuario.setPassword("contrasenia");
+		return usuario;
+	}
+
+
+	public Usuario editarUsuario() {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("César");
+		usuario.setApellido("Vargas");
+		usuario.setEmail("cesar@correo.com");
+		usuario.setTelefono("5568000916");
+		usuario.setPassword("contrasenia");
+		return usuario;
+	}
+
+
+	public Usuario eliminarUsuario() {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("César");
+		usuario.setApellido("Vargas");
+		usuario.setEmail("cesar@correo.com");
+		usuario.setTelefono("5568000916");
+		usuario.setPassword("contrasenia");
+		return usuario;
+	}
 }
